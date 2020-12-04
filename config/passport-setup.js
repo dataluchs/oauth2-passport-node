b53d729ccc5e0554cf03ea83836aa757d7306714
dataluchs <tim_stehle@posteo.de>
2020-12-05 00:08:43 +0100

    add some boilerplate

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,8 +1,40 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20')
+const FacebookStrategy = require('passport-facebook')
+const TwitterStrategy = require('passport-twitter')
 
-passport.use(new GoogleStrategy({
-    // options for strategy here
-}), () => {
-    // here passport callback function
-})
+// google strategy
+passport.use(
+    new GoogleStrategy({
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: '/auth/google/callback'
+    }),
+    function (accessToken, refreshToken, profile, cb) {
+        // handle this
+    }
+)
+
+
+// facebook strategy
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: '/auth/facebook/callback'
+},
+    function (accessToken, refreshToken, profile, cb) {
+        // handle this
+    }
+));
+
+
+// twitter strategy
+passport.use(new TwitterStrategy({
+    consumerKey: process.env.TWITTER_CONSUMER_KEY,
+    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+    callbackURL: '/auth/twitter/callback'
+},
+    function (token, tokenSecret, profile, cb) {
+        // handle this
+    }
+));
